b0f29a74a3bb9da88902788276b2905e7246fb36
barrett rose <roseba@debian>
2023-12-19 15:34:16 -0500

    Just trying to make it read from serial.

--- src/main.rs
+++ src/main.rs
@@ -13,8 +13,24 @@ fn main()  {
     
     let mut _count = 0u8;
     let mut _prev = Instant::now();
+    let mut line:Vec<u8> = String::from("line").into_bytes();
+     println!("line= {:?}", &line);
     loop {
-	println!("line");
+	match  file.num_ready_bytes(){
+	    Ok(_n) => {
+		if _n > 0{
+		    line.reserve(_n.try_into().unwrap() );
+		    println!("{:?} {:?}", _n, line.capacity() );
+		   //let _o = file.read_to_end(&mut line).unwrap();
+		   //println!("{:?}",_o);
+		    //println!("{:?}", String::from_utf8(line.clone() ).unwrap() );
+		}
+
+	    },
+	    Err(_e) => panic!("e = {:?}", _e),
+
+	};
+	//line.clear();
     }
 
 
