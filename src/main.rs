35fa3bf7006b7ad260fb8424883a7c05b1ff1b93
barrett rose <roseba@debian>
2023-12-22 13:22:00 -0500

    Still try to read character device.

--- src/main.rs
+++ src/main.rs
@@ -6,24 +6,28 @@ use opencv::{
     highgui
 };
 use char_device::*;
-use std::io::{Read,Write};
+//use std::io::{Read,Write};
+use std::io::prelude::*;
 
 fn main()  {
     let mut file = CharDevice::open("/dev/ttyUSB0").expect("open error!!");
     
     let mut _count = 0u8;
     let mut _prev = Instant::now();
-    let mut line:Vec<u8> = String::from("line").into_bytes();
-     println!("line= {:?}", &line);
+    //let mut line:Vec<u8> = Vec::new();
+    //print!("linecap={:?} ", line.capacity() );
+    //println!("line= {:?}", &line);
+    
     loop {
 	match  file.num_ready_bytes(){
 	    Ok(_n) => {
 		if _n > 0{
-		    line.reserve(_n.try_into().unwrap() );
-		    println!("{:?} {:?}", _n, line.capacity() );
-		   //let _o = file.read_to_end(&mut line).unwrap();
-		   //println!("{:?}",_o);
-		    //println!("{:?}", String::from_utf8(line.clone() ).unwrap() );
+		    print!("avail={:?} ", _n );
+		    let mut l:Vec<u8> = Vec::new();
+		    l.resize(_n.try_into().unwrap(),0);
+		    file.read(l.as_mut_slice() ).expect("red error");
+		    println!("line={:?}",String::from_utf16(l.as_slice() as &[u16] ) );
+		   //println!("{:?}", String::from_utf8(line.clone() ).unwrap() );
 		}
 
 	    },
