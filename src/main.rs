527f189e4db8b9f3c967ca3130e47da3a836a829
barrett rose <roseba@debian>
2024-01-09 18:49:19 -0500

    Not really working, but all the pieces are in place.

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,87 @@
-use std::time::{Duration, Instant};
-use opencv::{
-    core::Vector,
-    prelude::*,
-    videoio,
-    highgui
-};
-use char_device::*;
-use std::io::{Read,Write};
+#![no_std]
+#![no_main]
 
-fn main()  {
-    let mut file = CharDevice::open("/dev/ttyUSB0").expect("open error!!");
-    
-    let mut _count = 0u8;
-    let mut _prev = Instant::now();
-    let mut line:Vec<u8> = String::from("line").into_bytes();
-     println!("line= {:?}", &line);
-    loop {
-	match  file.num_ready_bytes(){
-	    Ok(_n) => {
-		if _n > 0{
-		    line.reserve(_n.try_into().unwrap() );
-		    println!("{:?} {:?}", _n, line.capacity() );
-		   //let _o = file.read_to_end(&mut line).unwrap();
-		   //println!("{:?}",_o);
-		    //println!("{:?}", String::from_utf8(line.clone() ).unwrap() );
-		}
+use panic_halt as _;
+use cortex_m_rt::entry;
 
-	    },
-	    Err(_e) => panic!("e = {:?}", _e),
+use ambiq_hal as hal;
+use hal::prelude::*;
+use embedded_hal::spi;
+use ufmt::uwrite;
+use ufmt::uwriteln;
 
-	};
-	//line.clear();
-    }
+use bit_byte_structs::bus::{Interface, InterfaceError};
 
+extern crate icm20948;
+use icm20948::ICMCommon;
+/*
+#[panic_handler]
+fn p(&PanicInfo) -> ! {
 
 }
+*/
+#[entry]
+fn main() -> ! {
+    let mut dp =  hal::pac::Peripherals::take().unwrap();
+    let core =  hal::pac::CorePeripherals::take().unwrap();
+
+    let mut delay = hal::delay::Delay::new(core.SYST, &mut dp.CLKGEN);
+
+    let pins = hal::gpio::Pins::new(dp.GPIO);
+    let mut led = pins.d19.into_push_pull_output();
+    let mut icm_pwr = pins.d18.into_push_pull_output();
+    let mut icm_cs = pins.icmcs.into_push_pull_output();
+
+    // set up serial
+    let mut serial = hal::uart::Uart0::new(dp.UART0, pins.tx0, pins.rx0);
+    uwriteln!(serial, "\rSetting up SPI..").unwrap() ;
+    let mut spi = hal::spi::Spi0::new(
+        dp.IOM0,
+        pins.d12,
+        pins.d13,
+        pins.d11,
+        hal::spi::Freq::F400kHz,
+        spi::MODE_0,
+    );
+    uwriteln!(serial, "\rSetting up ICM..").unwrap();
+    icm_pwr.set_high().unwrap();
+    let mut icm = icm20948::ICMSPI::new(&mut spi, &mut icm_cs).unwrap();
+    match icm.init(&mut spi, &mut icm_cs, &mut delay){
+	    Ok(_i) => uwriteln!(serial, "\rInitialized SUCCESS").unwrap(),
+	    Err(_e) => {
+		uwriteln!(serial, "\rInitialized ERROR ").unwrap();
+		panic!();
+	    },
+	};
+    //let mut icm_com = ICMCommon::new().unwrap();
+    //icm_com.init(&mut spi, &mut delay).unwrap();
+    
+    // Set up RTC
+    let mut rtc = hal::rtc::Rtc::new(dp.RTC, &mut dp.CLKGEN);
+    rtc.enable();
+    let mut timestamp = rtc.now().timestamp_millis();
+    loop {
+        led.toggle().unwrap();
+        delay.delay_ms(100u32);
+        let now = rtc.now().timestamp_millis();
+	uwrite!(serial, "\rloop ").unwrap();
+	
+	/*
+let (xa, ya, za, xg, yg, zg) =   match icm.get_values_accel_gyro(&mut spi, &mut icm_cs){
+	    Ok(_ag) => {
+		uwriteln!(serial, "SUCCESS").unwrap();
+		_ag
+	    },
+	    Err(_e) => {
+		uwriteln!(serial, "ERROR ").unwrap();
+		panic!();
+	    },
+	};
+		*/
+	//let (xa, ya, za, xg, yg, zg) =   icm.scale_raw_accel_gyro(icm.get_values_accel_gyro(&mut spi, &mut icm_cs).unwrap());
+	//uwriteln!(serial, "\r{}, {}, {}, {}, {}, {}", xa, ya, za, xg, yg, zg).unwrap() ;
+	uwriteln!(serial, "\r{}", now).unwrap();
+        timestamp = now;
+    }
+}
+
