03bb3dbae14a4682d6c71a42a6ca8a3ed7a9eb66
barrett rose <barrett.rose@gmail.com>
2024-02-19 13:55:47 -0500

    Random, varied changes.

--- src/imu.rs
+++ src/imu.rs
@@ -1,23 +1,95 @@
-use std::fmt::Debug;
+//use std::ops::Sub;
+//use std::fmt::{Debug,Formatter};
 use std::error::Error;
+use quaternion_core::{Vector3, Quaternion, normalize};
 
-#[derive(Debug)]
-pub struct Imu{
-    pub micros:u64,
-    pub x:f32,
-    pub y:f32,
-    pub z:f32,
+#[derive(Copy,Clone)]
+pub struct Ypr{
+ pub yaw:f32,
+ pub pitch:f32,
+ pub roll:f32,
+}
+#[derive(Copy,Clone)]
+pub struct Quat{
+    pub q0:f32,
+    pub q1:f32,
+    pub q2:f32,
+    pub q3:f32,
+}
+pub fn new ( fields:&Vec<&str> ) -> Result<Quaternion<f32>, Box<dyn Error>> 
+{
+	let q0:f32 = fields[0].parse::<f32>()?;
+	let q1:f32 = fields[1].parse::<f32>()?;
+	let q2:f32 = fields[2].parse::<f32>()?;
+	let q3:f32 = fields[3].parse::<f32>()?;
+	Ok(normalize( (q0,[q1,q2,q3]) as Quaternion<f32>))
+		
+}
+pub fn convert_ypr(q :Quaternion<f32>) -> Result<Vector3<f32>,Box<dyn Error>>
+{
+    let q0 :f32 = q.0;
+    let q1 :f32 = q.1[0];
+    let q2 :f32 = q.1[1];
+    let q3 :f32 = q.1[2];
+	let y1 :f32 = 2f32 * q0*q2 - 2f32 * q1*q3;
+	let y2 :f32 = 1f32 - q2*q2 - q3*q3 ;
+	let y :f32 =  (y1 as f64).atan2(y2 as f64).to_degrees() as f32;
+	
+	let p :f32 = ((2f32 * q0*q3 + 2f32 * q1*q2) as f64).asin().to_degrees() as f32;
+	
+	let r1 :f32 = 2f32 * q1*q3 - 2f32 * q2*q3;
+	let r2 :f32 = 1f32 - 2f32 * q3*q3  - 2f32 * q1*q1 ;
+	let r :f32 = (r1 as f64).atan2(r2 as f64).to_degrees() as f32;
+
+	Ok([y,p,r] as Vector3<f32>)
 }
 
-impl Imu{
-    pub fn new ( line:&String) -> Result<Imu, Box<dyn Error>> 
+/*
+
+impl Ypr{
+    pub fn diff(&self, other :&Ypr) -> Result<Ypr, Box<dyn Error>>
     {
-		let fields:Vec<&str> = line.split(",").collect();
-		Ok(Imu{
-		micros : fields[1].parse::<u64>()?,
-		x: fields[2].parse::<f32>()?,
-		y: fields[3].parse::<f32>()?,
-		z: fields[4].parse::<f32>()?,
-		})
+	Ok(Ypr{
+	    yaw:(self.yaw - other.yaw),
+	    pitch:(self.pitch - other.pitch),
+	    roll:(self.roll - other.roll),
+	})
     }
 }
+
+impl Debug for Ypr{
+        fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+	write!(f,"{1:.0$} {2:.0$} {3:.0$}",3,self.yaw,self.pitch,self.roll)
+    }
+}
+impl Debug for Quat{
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+	write!(f,"{1:.0$} {2:.0$} {3:.0$} {4:.0$} ",3,self.q0,self.q1,self.q2,self.q3)
+    }
+
+}
+
+
+impl Ypr{
+    pub fn diff(&self, other :&Ypr) -> Result<Ypr, Box<dyn Error>>
+    {
+	Ok(Ypr{
+	    yaw:(self.yaw - other.yaw),
+	    pitch:(self.pitch - other.pitch),
+	    roll:(self.roll - other.roll),
+	})
+    }
+}
+
+impl Debug for Ypr{
+        fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+	write!(f,"{1:.0$} {2:.0$} {3:.0$}",3,self.yaw,self.pitch,self.roll)
+    }
+}
+impl Debug for Quat{
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+	write!(f,"{1:.0$} {2:.0$} {3:.0$} {4:.0$} ",3,self.q0,self.q1,self.q2,self.q3)
+    }
+
+}
+*/
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,11 @@
-//use std::num::ErrorKind;
-
-use serialport::{
-    //SerialPortBuilder,
-//		 DataBits,Parity,FlowControl,StopBits,
-//		 TTYPort,
-		 SerialPort};
+use serialport::SerialPort;
 use std::io::{self,Read,Write};
-use core::time::Duration;
+use std::time::{Duration,Instant};
+use quaternion_core::{Vector3,
+		      Quaternion,
+		      RotationType::*,
+		      RotationSequence::XYZ,
+		      to_euler_angles, sub, norm};
 
 mod imu;
 
@@ -15,8 +14,13 @@ fn main() {
 	.timeout(Duration::from_millis(10))
 	//.data_bits(DataBits::Eight).parity(Parity::None).stop_bits(StopBits::One).flow_control(FlowControl::None)
 	.open_native().expect("Failed to open port");
+    let mut count:u32 = 0;
     let mut buf: Vec<u8> = vec![0; 100];
     let mut line_str = String::new();
+    let mut last = Instant::now();
+    let mut q :Quaternion<f32> = (1f32,[0f32,0f32,0f32]);
+    let mut ypr :Vector3<f32>  = [0f32,0f32,0f32];
+    let mut ypr_last :Vector3<f32>  = [0f32,0f32,0f32];
     loop{
 	let avail =  port.bytes_to_read().unwrap();
 	if avail == 0 {continue;}
@@ -28,23 +32,43 @@ fn main() {
 		let this_chunk = match std::str::from_utf8(&buf){
 		    Ok(_s) => _s,
 		    Err(_e) => {
+			#[cfg(feature = "debug")]
 			println!("to string error");
 			continue;
 		    },
 		};
 		line_str.push_str(this_chunk);		
 		if  buf.last() == Some(&10u8) {  // 10=ascii \n
-		    if line_str.len() > 47 && line_str.contains(','){
-			
-			let i = match imu::Imu::new(&line_str){
-			Ok(_i) => _i,
-			Err(_e) => {
-			    eprintln!("{:?}", _e);
-			    imu::Imu{micros:0u64,x:0f32,y:0f32,z:0f32}
-			},
-		    };
-			
-			println!("{:?}",i);	
+		    let _ = line_str.pop().unwrap();
+		    if  line_str.contains(','){	
+			let fields:Vec<&str> = line_str.split(",").collect();
+			if fields.len() == 4{
+			    q = match imu::new(&fields){
+				Ok(_i) => _i,
+				Err(_e) => {
+				    #[cfg(feature = "debug")]
+				    eprintln!("{:?}", _e);
+				    (1f32,[0f32,0f32,0f32]) as Quaternion<f32>
+				},
+			    };
+			}
+			count += 1;
+			ypr_last = ypr;
+			ypr = to_euler_angles(Intrinsic,XYZ,q);
+			let ypr1 = imu::convert_ypr(q).unwrap();
+			let diff:Vector3<f32> = sub(ypr , ypr_last);
+			//if diff[0].abs() >= 0.003f32{
+			    //#[cfg(feature = "debug")]
+			    println!("{:>7.4} {:>7.4}", ypr[0],ypr1[0]);
+			//}
+			//let test:f32 = q.q1*q.q2 + q.q0*q.q3;
+			//if test.abs() > 0.499f32{println!("{:?}",test);}
+			//println!("{:?}",q);
+		    }		    
+		    if last.elapsed() > Duration::from_millis(1000){
+			last = Instant::now();
+			//println!("{:?}", count);
+			count = 0;
 		    }
 		    line_str.clear();
 		    continue;
@@ -52,7 +76,10 @@ fn main() {
 	
 	    },
                     Err(ref e) if e.kind() == io::ErrorKind::TimedOut => (),
-                    Err(e) => eprintln!("{:?}", e),
+            Err(_e) => {
+		#[cfg(feature = "debug")]
+		eprintln!("{:?}", _e)
+	    },
 	    }
     }
 }
